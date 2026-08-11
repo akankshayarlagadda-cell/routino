dea4740ef7c163a9f757352b3ee04b632f7670ee
akanksha <akankshayarlagadda@gmail.com>
2026-08-11 22:31:42 +0530

    going to put main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks;
+
+    cout << "Enter Youur Marks: ";
+    cin >> marks;
+
+    if (marks >= 90)
+    {
+        cout << "Grade A"
+    }
+    else if (marks >= 80)
+    {
+        cout << "Grade B"
+    }
+    else if (marks >= 70)
+    {
+        cout << "Grade C"
+    }
+}
